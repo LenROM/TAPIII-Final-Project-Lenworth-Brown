a515221f5952ddd3c22731d1e89b8bc9540262ee
LenROM <lenworthbro@gmail.com>
2024-06-13 10:18:12 -0500

    Register

--- src/app/auth/register/register.component.ts
+++ src/app/auth/register/register.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AuthService } from 'src/app/app-services/auth.service';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.css']
 })
-export class RegisterComponent {
+export class RegisterComponent implements OnInit{
+
+
+  constructor(private authServe: AuthService, private router: Router){}
+
+  ngOnInit(): void {
+  }
+
+  saveUser(oForm: NgForm){
+    const addSub = this.authServe.register(oForm.value).subscribe( (res) =>{
+      if (res['status'] == 'success'){
+        this.router.navigateByUrl('/login');
+      }
+    })
+  }
 
 }
